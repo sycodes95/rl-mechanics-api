5722045653624c25af63cd73d79fcb57210cc752
sycodes95 <sycodes95@gmail.com>
2023-05-11 02:05:00 -0700

    added post end point for mechanic status

---
+++ controllers/mechanicsStatusController.js
@@ -0,0 +1,28 @@
+const pool = require('../db');
+
+exports.mechanics_post = (req, res, next) => {
+  const queryText = `
+  INSERT INTO mechanics_status 
+  (
+   user_id,
+   mech_id,
+   mechanic_status_value,
+  )
+  VALUES ($1, $2, $3)
+  RETURNING *
+  `;
+
+  const body = req.body;
+  const values = [
+    body.user_id, 
+    body.mech_id, 
+    body.mechanic_status_value
+  ]; 
+  pool.query(queryText, values, (err, result) => {
+
+    if(err) return res.json(err)
+
+    return res.json({mechanic: result.rows[0]})
+      
+  })
+}
